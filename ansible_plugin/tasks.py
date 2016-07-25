724e31a326f313cb06de1b3ceadaafd575327d3a
dongkai <dongkai@beyondcent.com>
2016-07-25 14:50:35 +0800

    ansible

--- ansible_plugin/tasks.py
+++ ansible_plugin/tasks.py
@@ -23,17 +23,18 @@ def ansible_playbook_ips(playbooks, inventory=list(), **kwargs):
 
 
 @operation
-def ansible_playbook_file(playbooks, inventory, **kwargs):
+def ansible_playbook_file(playbooks, inventorys, **kwargs):
     """ Runs a playbook as part of a Cloudify lifecycle operation """
 
-    inventory_path = utils.get_file(inventory)
-    ctx.logger.info('Inventory path: {0}.'.format(inventory_path))
+    for inventory in inventorys:
+        inventory_path = utils.get_file(inventory)
+        ctx.logger.info('Inventory path: {0}.'.format(inventory_path))
 
-    for playbook in playbooks:
-        playbook_path = utils.get_file(playbook)
-        ctx.logger.info('Playbook path: {0}.'.format(playbook_path))
-        command = ['ansible-playbook', '-i', inventory_path, playbook_path]
-        ctx.logger.info('Running command: {0}.'.format(command))
-        output = utils.run_command(command)
-        ctx.logger.info('Command Output: {0}.'.format(output))
-        ctx.logger.info('Finished running the Ansible Playbook.')
+        for playbook in playbooks:
+            playbook_path = utils.get_file(playbook)
+            ctx.logger.info('Playbook path: {0}.'.format(playbook_path))
+            command = ['ansible-playbook', '-i', inventory_path, playbook_path]
+            ctx.logger.info('Running command: {0}.'.format(command))
+            output = utils.run_command(command)
+            ctx.logger.info('Command Output: {0}.'.format(output))
+            ctx.logger.info('Finished running the Ansible Playbook.')
