e6722c4bab05f9fa2fbe47260a7b2e38dea85a5c
dongkai <dongkai@beyondcent.com>
2016-07-25 10:18:30 +0800

    ansible

--- ansible_plugin/tasks.py
+++ ansible_plugin/tasks.py
@@ -5,27 +5,27 @@ from ansible_plugin import utils
 from cloudify.decorators import operation
 
 
+# @operation
+# def ansible_playbook1(playbooks, inventory, **kwargs):
+#     """ Runs a playbook as part of a Cloudify lifecycle operation """
+#
+#     inventory_path = utils.get_inventory_path(inventory)
+#     ctx.logger.info('Inventory path: {0}.'.format(inventory_path))
+#
+#     for playbook in playbooks:
+#         playbook_path = utils.get_playbook_path(playbook)
+#         ctx.logger.info('Playbook path: {0}.'.format(playbook_path))
+#         command = ['ansible-playbook', '-i', inventory_path, playbook_path]
+#         ctx.logger.info('Running command: {0}.'.format(command))
+#         output = utils.run_command(command)
+#         ctx.logger.info('Command Output: {0}.'.format(output))
+#         ctx.logger.info('Finished running the Ansible Playbook.')
+#
+#     return "install success"
+
+
 @operation
-def ansible_playbook1(playbooks, inventory, **kwargs):
-    """ Runs a playbook as part of a Cloudify lifecycle operation """
-
-    inventory_path = utils.get_inventory_path(inventory)
-    ctx.logger.info('Inventory path: {0}.'.format(inventory_path))
-
-    for playbook in playbooks:
-        playbook_path = utils.get_playbook_path(playbook)
-        ctx.logger.info('Playbook path: {0}.'.format(playbook_path))
-        command = ['ansible-playbook', '-i', inventory_path, playbook_path]
-        ctx.logger.info('Running command: {0}.'.format(command))
-        output = utils.run_command(command)
-        ctx.logger.info('Command Output: {0}.'.format(output))
-        ctx.logger.info('Finished running the Ansible Playbook.')
-
-    return "install success"
-
-
-@operation
-def ansible_playbook(**kwargs):
+def ansible_playbook():
     """
     run a command
     :param ip:
@@ -45,6 +45,7 @@ def ansible_playbook(**kwargs):
     output = utils.run_command(command)
     ctx.logger.info('Command Output: {0}.'.format(output))
     ctx.logger.info('Finished running the Ansible Command.')
+    return "install success"
 
 
 
